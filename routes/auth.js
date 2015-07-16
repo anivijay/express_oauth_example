723ee4047e7977daa9154cdcde7bb0613ef28b72
Ani Vijay <ani@snappshr.com>
2015-07-16 12:03:54 -0700

    fixed up code in auth.js to get userinfo.email from google api

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.route('/google/callback')
 router.route('/google')
     .get(passport.authenticate('google', {
         scope: ['https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/.login']
+            'https://www.googleapis.com/auth/userinfo.email']
     }));
 
 
