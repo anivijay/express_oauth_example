54bb29ba95bec5456e182ff28b17a816342477af
Ani Vijay <ani@snappshr.com>
2015-07-21 16:30:29 -0700

    added facebook image download service

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var facebook = require('../services/facebook')()
+var facebook = require('../services/facebook')('1643147099255755','90447eba805563a3bfdec82a464330e3')
 
 router.use('/', function(req, res, next){
 
@@ -12,7 +12,18 @@ router.use('/', function(req, res, next){
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-    res.render('users', {user: req.user});
+
+    if(req.user.facebook){
+        facebook.getImage(req.user.facebook.token,
+            function(results){
+                req.user.facebook.image = results.url;
+                res.render('users', {user: req.user});
+        })
+    }
+    else{
+        res.render('users', {user: req.user});
+    }
+
 });
 
 module.exports = router;
